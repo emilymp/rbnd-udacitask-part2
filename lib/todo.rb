3f05d07d4d034ae7829c1d3727a1eb39f979ff0c
Emily Peri <emilyperi2@gmail.com>
2016-02-03 23:07:12 -0800

    added dependencies to gemfile, made error classes

--- lib/todo.rb
+++ lib/todo.rb
@@ -6,10 +6,21 @@ class TodoItem
     @description = description
     @due = options[:due] ? Chronic.parse(options[:due]) : options[:due]
     @priority = options[:priority]
+    raise UdaciListErrors::InvalidPriorityValue, "#{options[:priority]} is not a valid priority value" unless priority_valid
+    
   end
+
   def details
     format_description(@description) + "due: " +
     format_date(start_date: @due, str: "No due date") +
     format_priority(@priority)
   end
+
+  private
+
+  def priority_valid
+    valid = (%w(high medium low).include? @priority) || !@priority
+    return valid
+  end
+
 end
--- lib/udacilist.rb
+++ lib/udacilist.rb
@@ -5,15 +5,22 @@ class UdaciList
     @title = options[:title] || "Untitled List"
     @items = []
   end
+  
   def add(type, description, options={})
     type = type.downcase
-    @items.push TodoItem.new(description, options) if type == "todo"
-    @items.push EventItem.new(description, options) if type == "event"
-    @items.push LinkItem.new(description, options) if type == "link"
+    new_object = validity_check(type, description, options)
+    if new_object 
+      @items.push(new_object)
+    else 
+      raise UdaciListErrors::InvalidItemType, "#{type} not a valid type of item"
+    end
   end
+
   def delete(index)
+    raise UdaciListErrors::IndexExceedsListSize, "#{index} exceeds list size of #{@items.length}" if index > @items.length
     @items.delete_at(index - 1)
   end
+
   def all
     puts "-" * @title.length
     puts @title
@@ -22,4 +29,15 @@ class UdaciList
       puts "#{position + 1}) #{item.details}"
     end
   end
+
+  private
+
+  def validity_check(type, description, options)
+    new_object = false
+    new_object = TodoItem.new(description, options) if type == "todo"
+    new_object = EventItem.new(description, options) if type == "event"
+    new_object = LinkItem.new(description, options) if type == "link"
+    return new_object 
+  end
+
 end
